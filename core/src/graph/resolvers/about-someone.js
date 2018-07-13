57518f7cb82e045ef0bd7b0627f3691429ab0310
Gustavo García <guzgki@gmail.com>
2018-07-13 14:52:59 -0500

    remove some errors

--- core/src/graph/resolvers/about-someone.js
+++
@@ -1,8 +0,0 @@
-import signale from 'signale';
-import * as UserService from '../../services/user';
-
-export default (_, args) => {
-  const { email } = args;
-  signale.info(`Searching user by: ${email}`);
-  return UserService.findByEmail(email);
-};
