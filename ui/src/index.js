2ec1cab0406a5a3d881b073bca48767b48952f8c
Gustavo García <guzgki@gmail.com>
2018-07-13 13:45:46 -0500

    new compiled

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,13 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
 import './index.css';
+import ApolloClient from './graph/client';
 import App from './components/app';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ApolloProvider client={ApolloClient}>
+      <App />
+    </ApolloProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
