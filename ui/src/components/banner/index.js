193feca4fcb23e9a58d79d373afa322aaa6898cf
Gustavo García <guzgki@gmail.com>
2018-07-13 10:31:04 -0500

    banner component

---
+++ ui/src/components/banner/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class extends React.Component {
+  render() {
+    return (
+      <section className="banner">
+        <span className="text">Buy with confidence.</span>
+        <span className="text light">Browse our store and discover awesome stuff.</span>
+        <span className="text danger">Start Buying now!</span>
+      </section>
+    );
+  }
+}
