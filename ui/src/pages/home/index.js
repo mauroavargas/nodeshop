b0eb7eb80a858b2a955fda44b3be1f29f90a0f7c
Gustavo García <guzgki@gmail.com>
2018-07-13 10:31:16 -0500

    home page

---
+++ ui/src/pages/home/index.js
@@ -0,0 +1,11 @@
+/* eslint no-unused-vars: off */
+import React from 'react';
+import Banner from '../../components/banner';
+
+export default class extends React.Component {
+  render() {
+    return ([
+      <Banner key="b4nn3r" />
+    ]);
+  }
+}
