e310c9302a5bd9eb6ef35ce3397610f76e7fbe81
Gustavo García <guzgki@gmail.com>
2018-07-13 10:22:38 -0500

    check if the user is already register first

--- core/src/services/user.js
+++ core/src/services/user.js
@@ -4,6 +4,14 @@ export const findByToken = (token) => {
   return User.findOne({ token });
 };
 
-export const create = (data) => {
+export const findByEmail = (email) => {
+  return User.findOne({ email });
+};
+
+export const create = async (data) => {
+  const user = await findByEmail(data.email);
+  if (user) {
+    throw new Error('The user is already registered.');
+  }
   return User.create(data);
 };
