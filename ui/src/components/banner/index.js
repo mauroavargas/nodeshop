4c5f81d4b93ec6833d1a865ad29e048178d29e29
Gustavo García <guzgki@gmail.com>
2018-07-16 10:03:42 -0500

    refactor banner

--- ui/src/components/banner/index.js
+++ ui/src/components/banner/index.js
@@ -1,12 +1,14 @@
+/* eslint no-unused-vars: off */
 import React from 'react';
+import Text from '../text';
 
 export default class extends React.Component {
   render() {
     return (
       <section className="banner">
-        <span className="text">Buy with confidence.</span>
-        <span className="text light">Browse our store and discover awesome stuff.</span>
-        <span className="text danger">Start Buying now!</span>
+        <Text color="gray" content="Buy with confindence" style={{ fontSize: 15 }}/>
+        <Text weight="light" content="Browse our store and discover awesome stuff." style={{ fontSize: 15 }}/>
+        <Text color="danger" content="Start Buying now!" style={{ fontSize: 15 }}/>
       </section>
     );
   }
