d6904bd0e15b593a5f187f973216334b927b9a67
Gustavo García <guzgki@gmail.com>
2018-07-16 10:00:54 -0500

    rename error

--- core/src/errors/bad-request.js
+++ core/src/errors/bad-request.js
@@ -1,5 +1,5 @@
 import { createError } from 'apollo-errors';
 
-export default createError('BadRequest', {
+export default createError('BadRequestError', {
   message: 'Something went wrong with your request'
 });
