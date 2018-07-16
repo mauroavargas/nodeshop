127e06b6cd0186eb353f55b3e12ae2c872da5cd3
Gustavo García <guzgki@gmail.com>
2018-07-16 10:09:31 -0500

    new builds

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,18 +2,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { ApolloProvider } from 'react-apollo';
-import './index.css';
-import ApolloClient from './graph/client';
+// import { ApolloProvider } from 'react-apollo';
+// import ApolloClient from './graph/client';
+import { Provider } from './store';
 import App from './components/app';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
-    <ApolloProvider client={ApolloClient}>
-      <App />
-    </ApolloProvider>
+    <Provider>
+      <App/>
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );
 registerServiceWorker();
+
+/*
+We need to use the ApolloProvider if we want to use Query component
+<ApolloProvider client={ApolloClient}>
+  <App />
+</ApolloProvider>
+*/
