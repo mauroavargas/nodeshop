b48f9d670397ca436a7eb498b482c2b6440e3391
Gustavo García <guzgki@gmail.com>
2018-07-16 10:01:05 -0500

    remove log

--- core/src/graph/resolvers/about-me.js
+++ core/src/graph/resolvers/about-me.js
@@ -1,8 +1,5 @@
-import signale from 'signale';
 import * as UserService from '../../services/user';
 
 export default (_, args) => {
-  const { token } = args;
-  signale.info(`Token: ${token}`);
-  return UserService.findByToken(token);
+  return UserService.findByToken(args.token);
 };
