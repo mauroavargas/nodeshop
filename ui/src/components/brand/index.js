3fe8b0f875cf2fc45a56172c051af832046ee898
Gustavo García <guzgki@gmail.com>
2018-07-16 10:03:33 -0500

    refactor brand

--- ui/src/components/brand/index.js
+++ ui/src/components/brand/index.js
@@ -1,19 +1,20 @@
 /* eslint no-unused-vars: off */
 import React from 'react';
-import Logo from './logo';
-import Search from './search';
-import User from './user';
-import NotLogged from './not-logged';
+import Text from '../text';
 
 export default class extends React.Component {
   render() {
     return (
       <header className="brand">
-        <Search />
-        <Logo />
-        {
-          this.props.logged ? <User /> : <NotLogged />
-        }
+        <div>
+          <Text color="gray" weight="bold" content="❤" style={{ color: '#F22613', marginRight: 10 }}/>
+          <Text color="gray" weight="bold" content="By Gugadev"/>
+        </div>
+        <div>
+          <a href="https://github.com/gugadev/nodeshop">
+            <Text color="gray" weight="bold" content="Source code"/>
+          </a>
+        </div>
       </header>
     );
   }
