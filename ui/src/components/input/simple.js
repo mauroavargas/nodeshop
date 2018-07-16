3f3647bf24798781a86ad1bb3f1b644cd0f33647
Gustavo García <guzgki@gmail.com>
2018-07-16 10:04:19 -0500

    add onChange and name props

--- ui/src/components/input/simple.js
+++ ui/src/components/input/simple.js
@@ -9,6 +9,8 @@ export default class extends React.Component {
         valid={this.props.valid}
         touched={this.props.touched}
         placeholder={this.props.placeholder}
+        onChange={this.props.onChange}
+        name={this.props.name}
         className="input"
       />
     );
