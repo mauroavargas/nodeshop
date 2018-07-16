535d3e88bf408224c47cb2ed5ea5bc577a87c3dd
Gustavo García <guzgki@gmail.com>
2018-07-16 10:02:56 -0500

    [fix] get email from token decoded payload

--- core/src/services/user.js
+++ core/src/services/user.js
@@ -1,11 +1,33 @@
 import User from '../models/user';
+import * as Jwt from '../util/token';
+import { verify } from '../util/password';
 
 export const findByToken = (token) => {
-  return User.findOne({ token });
+  const { email } = Jwt.verify(token);
+  if (!email) {
+    return null;
+  }
+  return findByEmail(email);
 };
 
 export const findByEmail = (email) => {
   return User.findOne({ email });
+};
+
+export const login = async (email, password) => {
+  const user = await findByEmail(email);
+  if (user) {
+    try {
+      const match = await verify(password, user.password);
+      if (match) {
+        return user;
+      }
+      throw new Error('Incorrect username or password');
+    } catch (e) {
+      throw new Error('Incorrect username or password');
+    }
+  }
+  throw new Error('Incorrect username or password');
 };
 
 export const create = async (data) => {
