0bcf454b60358701f440ee18db40c7fae4d5181a
Gustavo García <guzgki@gmail.com>
2018-07-16 21:07:03 -0500

    change color

--- ui/src/components/app/banner/index.js
+++ ui/src/components/app/banner/index.js
@@ -6,7 +6,7 @@ export default class extends React.Component {
   render() {
     return (
       <section className="banner">
-        <Text color="gray" content="Buy with confindence" style={{ fontSize: 15 }}/>
+        <Text content="Buy with confindence" style={{ fontSize: 15 }}/>
         <Text weight="light" content="Browse our store and discover awesome stuff." style={{ fontSize: 15 }}/>
         <Text color="danger" content="Start Buying now!" style={{ fontSize: 15 }}/>
       </section>
